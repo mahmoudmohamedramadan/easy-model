3a935f00aa2fec789638ff97b501fb61d0dece56
mahmoudmohamedramadan <mahmoudramadan496@gmail.com>
2024-11-15 21:35:29 +0200

    Add the `Updatable` trait

---
+++ src/Updatable.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Ramadan\EasyModel;
+
+use Ramadan\EasyModel\Concerns\Update\HasModel as UpdatableModel;
+
+trait Updatable
+{
+    use UpdatableModel;
+
+    /**
+     * Create or update a record matching the attributes, and fill it with values.
+     *
+     * @param array $attributes
+     * @param array $values
+     * @param \Illuminate\Database\Eloquent\Model|string|null $model
+     * @return \Illuminate\Database\Eloquent\Model
+     */
+    public function updateOrCreate(array $attributes, array $values = [], $model = null)
+    {
+        if (!empty($model)) {
+            $this->setUpdatableModel($model);
+        }
+
+        return $this->getUpdatableModel()->updateOrCreate($attributes, $values);
+    }
+}
